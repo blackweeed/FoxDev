0a54fc34089456df26860ab818c93b7e5ea10679
blackweeed <blackweeeed@gmail.com>
2024-01-29 17:39:38 +0100

    disable eslint rule for build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	typescript: { ignoreBuildErrors: true },
+	eslint: { ignoreDuringBuilds: true },
 };
 
 export default withNextIntl(nextConfig);
